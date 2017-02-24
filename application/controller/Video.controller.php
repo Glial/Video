62f020a60174658d5ab7471688b91ccb003c3769
Esysteme <you@example.com>
2017-02-24 23:17:52 +0100

    Init

---
+++ application/controller/Video.controller.php
@@ -0,0 +1,27 @@
+<?php
+
+use \Glial\Synapse\Controller;
+use \Glial\I18n\I18n;
+
+class Video extends Controller
+{
+    var $rarbg = "https://www.rarbg.to/torrents.php";
+    
+    
+    
+    public function index()
+    {
+        
+    }
+    
+    
+    public function getLink()
+    {
+        
+    }
+            
+    
+    
+    
+    
+}
---
+++ configuration/db.config.php
@@ -0,0 +1,7 @@
+ <?php
+
+
+ if (! defined('DB_DEFAULT'))
+ {
+     define("DB_DEFAULT", "default");
+ }
